85abe885b79b5830845591e294c124d90596a4fe
pr0gramama <paleofemme@gmail.com>
2022-02-07 13:30:00 -0700

    Revised game - Mary Ann

--- jumper/game/matcher.py
+++ jumper/game/matcher.py
@@ -1,6 +1,8 @@
+import random
 
 class Matcher:
-    """The person looking for the coincidence of the letter in the word. 
+    """The user looking for the coincidence of the letter in the word. The responsibility
+        of the matcher is to guess the correct word before the parachute (Sketcher) disappears.
         
     Attributes:
         
@@ -12,10 +14,32 @@ class Matcher:
         Args:
             self (Matcher): An instance of Matcher.
         """
-        self._list = [] # start with 
+        list_words = random.choice(["dish", "chair", "cheese", "cat", "house", 
+            "car", "smell", "break", "seat", "ghost",])
+        self._word = []
+        for letter in list_words:
+            self._word.append(letter)
+
+        self.guesses = []
+        self._unknown_word = [] 
     
-    def get_match(self, list):
-        """Looks a matching.
+    def set_hyphens(self):
+        """ Loops through the letters in the word.  If the guesses is found in the word
+        print that letter, otherwise set it to an underscore."""
+        self._unknown_word.clear
+
+        for letter in self._word:
+            if letter in self.guesses:
+                self._unknown_word.append(letter)
+            else:
+                self._unknown_word.append("_")
+        return self._unknown_word
+    
+    def get_word(self, guess):
+        self.guesses.append(guess)
+
+    def get_match(self):
+        """If the guessed word is the random word, return True, otherwise the word is wrong.
 
         Args:
             self (Matcher): An instance of Matcher.
@@ -23,22 +47,33 @@ class Matcher:
         Returns:
             string: 
         """
-        pass
+        if self._unknown_word == self._word:
+           return True 
+        else:
+           return False
         
 
-    def is_found(self):
-        """Whether or not the total matching has been found.
+    def is_found(self, guess):
+        """If the guessed letter is found in the random word, return true, otherwise the 
+        guess is wrong
 
         Args:
             self (Matcher): An instance of Matcher.
             
         Returns:
-            boolean: True if the matcher was found; false if otherwise.
+        boolean: True if the matcher was found; false if otherwise.
         """
-        return True
+        for letter in self._word:
+            if letter == guess:
+                return True
+            else:
+                return False
 
-    def get_text(self):
-        """The text when the game end
-        """
-        pass
-        
+    def outcome(self, wrong, _unknown_word): # HELP! THIS IS NOT WORKING
+        """Gives the player 10 chances to guess the letters."""
+        if wrong >= 10:
+            return False
+        elif _unknown_word == self._word:
+            return True
+        else: 
+            return None
--- jumper/game/sketcher.py
+++ jumper/game/sketcher.py
@@ -1,4 +1,5 @@
-from game.words_service import Words
+from game.terminal_service import TerminalService
+
 class Sketcher:
     """The person drawing the parachute and hyphens acording to the coincidence in the word. 
     
@@ -11,8 +12,7 @@ class Sketcher:
         Args:
             self (Seeker): An instance of Seeker.
         """
-        self._word = Words()
-        self.word_guess = []
+        self._terminal_service = TerminalService()
         self.parachute = ["    ___   ",
                           "   /___\  ",
                           "   \   /  ",
@@ -24,26 +24,30 @@ class Sketcher:
                           "^^^^^^^^^^",
         ]
 
-    def set_hyphens(self,word):
-        """Draws the hyphens of the current word.
-        Returns:
-            string: The current word,
-        """
-        word = self._word.get_word()
-        print()
-        #print(word)
-        for i in range(len(word)):
-            self.word_guess.append("_")
-            print(self.word_guess[i], end=" ")
-        print()
+    def make_guess(self):
+        # Here is the input which is called in Director in get_inputs
+        guess = self._terminal_service.read_text("\nGuess a letter [a-z]: ")
+        return guess.lower()
 
-    def set_parachute(self,guess):
+    def set_parachute(self, number): 
         """Draw the parachute every time
         """
         for i in range(len(self.parachute)):
             print(self.parachute[i])
+
+    def get_text(self, won):
+        """Prints the output for the endgame, either "won" or "lost" """
         
-    def is_clean(self):
+        if won == True:
+                end_game = "  \\0/ \n   | \n__/_\__ \nYou win!"
+        else:
+            end_game = "   \ /\n___\|/___\n    x\nYou lose"
+        self._terminal_service.write_text(end_game)
+
+    
+    def is_clean(self):  
+        #This is not working.  Needs to clear the parachute line by line if
+        #is_found doesn't return True.
         """ True when the parachute is all clean
         """
         pass
