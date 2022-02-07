405014fd718a11b746311a16b420422a8fccaeb3
pr0gramama <paleofemme@gmail.com>
2022-02-07 13:30:50 -0700

    2nd revision Mary Ann

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,8 +1,6 @@
 from game.terminal_service import TerminalService
-from game.words_service import Words
 from game.sketcher import Sketcher
 from game.matcher import Matcher
-
 
 class Director:
     """A person who directs the game. 
@@ -12,7 +10,6 @@ class Director:
     Attributes:
         matcher (Matcher): The game's skeeer matching letter in the word.
         is_playing (boolean): Whether or not to keep playing.
-        words (Words): The word list handler
         sketcher (Seeker): The game's sketcher, parachute and word.
         terminal_service: For getting and displaying information on the terminal.
     """
@@ -24,13 +21,14 @@ class Director:
             self (Director): an instance of Director.
         """
         self._is_playing = True
-        self._words = Words()
         self._sketcher = Sketcher()
         self._matcher = Matcher()
         self._terminal_service = TerminalService()
-        self.letter = "" 
-        self.guess = []
-        
+        self._unknown_word = []  #container for unknown word chosen by random module -mary ann
+        self._wrong_guess = 1 #supposed to add up wrong guesses.  Not working (Matcher: outcome)
+        self.last_guess = "" #most recent letter guessed by user
+        self.outcome = None  #SEE Matcher function outcome.  This may be a problem.
+
     def start_game(self):
         """Starts the game by running the main game loop.
         
@@ -48,10 +46,8 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        word = self._words.get_word() #get the word from the list at the beginning of the game
-        self._sketcher.set_hyphens(word) # draws the dashes according to the number of letters, musts keep an updated list
-        self._sketcher.set_parachute(self.guess) # draws the parachute according to the game, must save the updated matrix
-        self.letter = self._terminal_service.read_text("\nGuess a letter [a-z]: ") #asks for a letter on the screen and saves it in an attribute. A get function is used to get it
+        self.last_guess = self._sketcher.make_guess()
+        self._matcher.get_word(self.last_guess)
         
     def _do_updates(self):
         """Check matching and change the lists, word and parachute.
@@ -59,7 +55,9 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        self.guess = self._matcher.get_match(self.letter)
+        self._unknown_word = self._matcher.set_hyphens()
+        if self._matcher.is_found(self.last_guess) == False:
+            self._wrong_guess += 1
         
     def _do_outputs(self):
         """Ask for input on the screen. Post the appropriate message if win or lose.
@@ -67,9 +65,10 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        text = self._matcher.get_text()
-        self._terminal_service.write_text(text)
-        if self._matcher.is_found():  #If the puzzle is solved the game is over.
-            self._is_playing = False
-        if self._sketcher.is_clean(): # If the parachute is clean the game is over
-            self._is_playing = False
+        self._sketcher.set_parachute(self._wrong_guess)
+        self._terminal_service.write_text(self._unknown_word)
+
+        self._is_playing = (self._matcher.outcome(self._wrong_guess, self._unknown_word)== None)
+        if self._is_playing == False:
+            self._sketcher.get_text(self._matcher.outcome(self._wrong_guess, self._unknown_word))
+        self._unknown_word.clear() 
